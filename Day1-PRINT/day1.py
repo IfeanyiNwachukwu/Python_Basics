c1e691e4d5b86207bafc7f68481b0c4a64a019f1
Wisdom Nwachukwu <wisdomn@talprosoftware.com>
2026-04-18 21:25:23 +0100

    day 1 lessons

---
+++ Day1-PRINT/day1.py
@@ -0,0 +1,28 @@
+# print("Hello world")
+
+# # String Manipulation
+# print("Hello World!\nHello World!\nHello World!")
+
+# # String Cocantenationc
+# print("Hello" + " " + "Angela")
+
+# # Inputs
+# input("What is your name?")
+
+# print("Hello " + input("What is your name?")+"!")
+
+# # Variables
+# Cairo
+# print("Hello" + " " + userName + "!")
+# print(userName)
+
+# String Function
+name = input("What is your name? ")
+
+print(name)
+
+length_of_characters = len(name)
+
+print("There are", length_of_characters, "characters in the inputted text")
+
+print(len(input("What is your name?")))
