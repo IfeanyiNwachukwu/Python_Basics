2dea13956f0f0d22a282673135861fb61887e12b
Wisdom Nwachukwu <wisdomn@talprosoftware.com>
2026-04-18 21:31:01 +0100

    Task COmpleted

---
+++ Day1-PRINT/taskDay1.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator Console")
+nameOfCity = input("What is the name of your city?")
+nameOfFavPet = input("What is the name of your favourite pet?")
+
+print("Your Band Name should be" + " " + nameOfCity + " " + nameOfFavPet)
